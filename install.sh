cefa9db5e60e5391bf47b25ac47aa2228a10051b
ITmail-dk <48917593+ITmail-dk@users.noreply.github.com>
2024-10-16 12:47:07 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -868,24 +868,87 @@ echo -e "${GREEN}Kitty Theme.conf Start${NC}"
 if [ ! -f $HOME/.config/kitty/themes/kittytheme.conf ]; then
 mkdir -p $HOME/.config/kitty/themes
 cat << "KITTYTHEMECONF" > $HOME/.config/kitty/themes/kittytheme.conf
-background #1b0200
-foreground #ee712d
-color0 #1b0200
-color1 #240002
-color2 #d74d00
-color3 #d74d00
-color4 #9c2101
-color5 #d74d00
-color6 #d74d00
-color7 #d74d00
-color8 #d74d00
-color9 #830508
-color10 #d74d00
-color11 #d74d00
-color12 #9a292f
-color13 #e46324
-color14 #ea6f10
-color15 #ee712d
+# vim:ft=kitty
+
+## name:     Catppuccin Kitty Mocha
+## author:   Catppuccin Org
+## license:  MIT
+## upstream: https://github.com/catppuccin/kitty/blob/main/themes/mocha.conf
+## blurb:    Soothing pastel theme for the high-spirited!
+
+
+
+# The basic colors
+foreground              #cdd6f4
+background              #1e1e2e
+selection_foreground    #1e1e2e
+selection_background    #f5e0dc
+
+# Cursor colors
+cursor                  #f5e0dc
+cursor_text_color       #1e1e2e
+
+# URL underline color when hovering with mouse
+url_color               #f5e0dc
+
+# Kitty window border colors
+active_border_color     #b4befe
+inactive_border_color   #6c7086
+bell_border_color       #f9e2af
+
+# OS Window titlebar colors
+wayland_titlebar_color system
+macos_titlebar_color system
+
+# Tab bar colors
+active_tab_foreground   #11111b
+active_tab_background   #cba6f7
+inactive_tab_foreground #cdd6f4
+inactive_tab_background #181825
+tab_bar_background      #11111b
+
+# Colors for marks (marked text in the terminal)
+mark1_foreground #1e1e2e
+mark1_background #b4befe
+mark2_foreground #1e1e2e
+mark2_background #cba6f7
+mark3_foreground #1e1e2e
+mark3_background #74c7ec
+
+# The 16 terminal colors
+
+# black
+color0 #45475a
+color8 #585b70
+
+# red
+color1 #f38ba8
+color9 #f38ba8
+
+# green
+color2  #a6e3a1
+color10 #a6e3a1
+
+# yellow
+color3  #f9e2af
+color11 #f9e2af
+
+# blue
+color4  #89b4fa
+color12 #89b4fa
+
+# magenta
+color5  #f5c2e7
+color13 #f5c2e7
+
+# cyan
+color6  #94e2d5
+color14 #94e2d5
+
+# white
+color7  #bac2de
+color15 #a6adc8
+
 
 KITTYTHEMECONF
 
